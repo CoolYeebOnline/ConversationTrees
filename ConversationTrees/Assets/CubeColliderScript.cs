4a9d7e2fe4cc3c88bf24fd09943b39c439fe8263
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2019-10-23 13:12:39 +0100

    October 23rd - Conversation Trees

---
+++ ConversationTrees/Assets/CubeColliderScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeColliderScript : MonoBehaviour
+{
+    private bool PlayerInRange = false;
+
+    private Text PlayerText;
+
+    ConversationClass currentNode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!PlayerInRange) return;
+
+        PlayerText.text = currentNode.Message;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentNode = currentNode.response1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentNode = currentNode.response2;
+        }
+
+        if (currentNode == null)
+        {
+            PlayerText.text = "";
+            PlayerInRange = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+
+        if (otherCollider.tag == "Player")
+        {
+            PlayerInRange = true;
+            PlayerText = GameObject.FindWithTag("PlayerText").GetComponent<Text>();
+            currentNode = ConversationTree;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D otherCollider)
+    {
+        if (otherCollider.tag == "Player")
+        {
+            PlayerInRange = false;
+            PlayerText.text = string.Empty;
+            PlayerText = null;
+            
+        }
+    }
+    private ConversationClass ConversationTree = new ConversationClass()
+    {
+        Message = "Hello, how are you today? \n [1 - Great, let's talk again], [2 - Okay, Goodbye",
+        response1 = new ConversationClass()
+        {
+            Message = "I'm fine thanks \n [1 - Great, let's talk again], [2 - Ok, Goodbye]",
+            response1 = new ConversationClass()
+            {
+                Message = "Great, we certainly will!",
+                response1 = null,
+                response2 = null
+            },
+            response2 = null
+        },
+        response2 = new ConversationClass()
+        {
+            Message = "Ok, Goodbye!",
+            response1 = null,
+            response2 = null
+        }
+    };
+}
---
+++ ConversationTrees/ConversationClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class ConversationClass
+    {
+        public string Message
+        {
+            get;
+            set;
+        }
+
+        public ConversationClass response1
+        {
+            get;
+            set;
+        }
+
+        public ConversationClass response2
+        {
+            get;
+            set;
+        }
+ }
+
+
